a71ca3afc65fef266a230d5fa1d780059c019cbb
ValeriyaBudarina <vdbudarina_3@edu.hse.ru>
2023-03-31 19:00:25 +0300

    Initial commit

---
+++ assets/js/script2.js
@@ -0,0 +1,25 @@
+var card = document.querySelector('.card2');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+var card2 = document.querySelector('.card3');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+var card3 = document.querySelector('.card4');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+var card4 = document.querySelector('.card5');
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+var card5 = document.querySelector('.card6');
+card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+});
+var card6 = document.querySelector('.card7');
+card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+});
